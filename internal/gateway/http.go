f8980d418219cadee87688a7399e93789a4e6c9e
Hiram Chirino <hiram@hiramchirino.com>
2020-04-28 21:44:52 -0400

    feat: Forward HTTP headers to the upstream endpoints
    fixes #1

---
+++ internal/gateway/http.go
@@ -0,0 +1,51 @@
+package gateway
+
+import (
+	"net"
+	"net/http"
+	"strings"
+
+	"github.com/chirino/graphql"
+	"github.com/chirino/graphql/relay"
+)
+
+func CreateHttpHandler(f graphql.ServeGraphQLStreamFunc) http.Handler {
+	return &relay.Handler{ServeGraphQLStream: f}
+}
+
+type proxyTransport byte
+
+func (p proxyTransport) RoundTrip(req *http.Request) (*http.Response, error) {
+
+	ctx := req.Context()
+	if ctx != nil {
+
+		value := ctx.Value("*net/http.Request")
+		if value != nil {
+			originalRequest := value.(*http.Request)
+
+			if clientIP, _, err := net.SplitHostPort(originalRequest.RemoteAddr); err == nil {
+				if prior, ok := originalRequest.Header["X-Forwarded-For"]; ok {
+					clientIP = strings.Join(prior, ", ") + ", " + clientIP
+				}
+				req.Header.Set("X-Forwarded-For", clientIP)
+			}
+
+			if _, ok := originalRequest.Header["X-Forwarded-Host"]; !ok {
+				if host := originalRequest.Header.Get("Host"); host != "" {
+					req.Header.Set("X-Forwarded-Host", host)
+				}
+			}
+
+			if _, ok := originalRequest.Header["X-Forwarded-Proto"]; !ok {
+				if originalRequest.TLS != nil {
+					req.Header.Set("X-Forwarded-Proto", "https")
+				} else {
+					req.Header.Set("X-Forwarded-Proto", "http")
+				}
+			}
+		}
+
+	}
+	return http.DefaultTransport.RoundTrip(req)
+}
