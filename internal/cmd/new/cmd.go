602f4c24d7e4de498a0920d4029fd04b36c4facb
Hiram Chirino <hiram@hiramchirino.com>
2020-05-23 10:31:12 -0400

    update the default config.

--- internal/cmd/new/cmd.go
+++ internal/cmd/new/cmd.go
@@ -35,21 +35,69 @@ func run(cmd *cobra.Command, args []string) {
 listen: localhost:8080
 
 ##
-## Example config to configure an upstream server and expose it's 
-## Query type via the gateway's Query type.
+## Some example configuration.... 
 ##
-# upstreams:
-#   anilist:
-#     url: https://graphql.anilist.co/
-#     prefix: Ani
-# 
-# types:
-#   - name: Query
-#     actions:
-#       - type: mount
-#         upstream: anilist
-#         query: query {}
-# 
+#upstreams:
+#  anilist:
+#    url: https://graphql.anilist.co
+#    prefix: Ani
+#  pokemon:
+#    url: https://graphql-pokemon.now.sh
+#    prefix: Pokemon
+#    
+#types:
+#  - name: Query
+#    actions:
+#      # mounts the root anilist query to the anime field
+#      - type: mount
+#        field: anime
+#        upstream: anilist
+#        query: query {}
+#
+#      # mounts the root pokemon query to the pokemon field
+#      - type: mount
+#        field: pokemon
+#        upstream: pokemon
+#        query: query {}
+#
+#  - name: AniCharacter
+#    actions:
+#      # mounts the root anilist query to the anime field
+#      - type: link
+#        field: pokemon
+#        vars:
+#          $fullname: name { full }
+#        upstream: pokemon
+#        query: query { pokemon(name:$fullname) }
+##
+## The above link lets you do queries that access data from both the anilist and pokemon services. Example:
+##
+##     query {
+##      anime {
+##        Character(search: "Pikachu") {
+##          description
+##          image {
+##            medium
+##          }
+##          pokemon {
+##            attacks {
+##              special {
+##                name
+##                type
+##                damage
+##              }
+##            }
+##          }
+##        }
+##      }
+##     }
+#
+#  - name: Mutation
+#    actions:
+#      # mounts all the fields of the root anilist mutation
+#      - type: mount
+#        upstream: anilist
+#        query: mutation {}
 
 `), 0644)
 
