78a4a4b25682e308ae3800a6e512250d745c752d
Ankit Hans <ankithans1947@gmail.com>
2021-02-16 12:34:57 +0530

    feat: add tailwindcss support

---
+++ website/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ["./src/**/*.html", "./src/**/*.js", "./src/**/*.tsx"],
+  darkMode: "media", // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
